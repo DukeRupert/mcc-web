420f05e26e7ac574b6d3621e8bac2d0d8f07033b
dukerupert <logan@fireflysoftware.dev>
2025-03-17 17:57:07 -0600

    Added flowbite

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
     import type { PageProps } from './$types';
-
+  import { Alert } from 'flowbite-svelte';
 	let { data }: PageProps = $props();
     console.log(data);
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="p-8">
+  <Alert>
+    <span class="font-medium">Info alert!</span>
+    Change a few things up and try submitting again.
+  </Alert>
+</div>
