1152bc5bc733f1e234846c02ef5b45a580d63801
dukerupert <logan@fireflysoftware.dev>
2025-03-20 20:22:23 +0000

    ui: added breadcrumbs to /creators

--- src/routes/creators/[slug]/+page.svelte
+++ src/routes/creators/[slug]/+page.svelte
@@ -2,9 +2,11 @@
 	import Helmet from './helmet.svelte'
     import Profile from '$lib/creator/profile.svelte';
 	import type { PageProps } from './$types';
+	import Breadcrumbs from '$lib/page/breadcrumbs.svelte';
 	let { data }: PageProps = $props();
 	let { creator } = data;
 </script>
 
 <Helmet {creator} />
+<Breadcrumbs />
 <Profile {creator} />
