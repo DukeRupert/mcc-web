d4a5cef97266f706f3ea1b365b406af78f682f51
dukerupert <logan@fireflysoftware.dev>
2025-03-20 19:40:53 +0000

    feature: categories/[slug] works

---
+++ src/routes/categories/[slug]/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { PageProps } from './$types';
+	let { data }: PageProps = $props();
+	let { slug, posts } = data;
+</script>
+
+<h1>{slug}</h1>
+{#each posts as post}
+	<h2>{post.title}</h2>
+{/each}
---
+++ src/routes/categories/[slug]/+page.ts
@@ -0,0 +1,23 @@
+import type { PageLoad } from './$types';
+import client from '$lib/directus/client';
+import { readItems } from '@directus/sdk';
+
+export const load: PageLoad = async ({ params }) => {
+    let slug = params.slug
+
+    let posts = await client.request(readItems('post', {
+        filter: {
+            category_id: {
+                slug: {
+                    _eq: slug
+                }
+            }
+        },
+        fields: ['*', { creator_id: ['title', 'slug', { avatar: ['id', 'height', 'width'] }] }, { image: ['id', 'width', 'height',] }, { category_id: ['*'] }]
+    }))
+
+    return {
+        slug,
+        posts
+    };
+};
